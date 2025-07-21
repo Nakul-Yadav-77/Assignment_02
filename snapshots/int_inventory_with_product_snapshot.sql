10d9dfcb415a685daf6c88f63eaa0063ae012edb
Nakul Yadav <Nakul.Yadav@thomsonreuters.com>
2025-07-21 13:29:31 +0000

    c3

--- snapshots/int_inventory_with_product_snapshot.sql
+++ snapshots/int_inventory_with_product_snapshot.sql
@@ -1,10 +1,12 @@
 {% snapshot int_inventory_with_product_snapshot %}
-{{ config(
-    target_schema='snapshots',
-    unique_key='product_id',  -- replace with your actual unique key
-    strategy='check',
-    check_cols=['store_id', 'Stock_on_hand']  -- replace with actual columns
-) }}
+    {{ 
+        config(
+            target_schema='snapshots',
+            unique_key='product_id',  
+            strategy='check',
+            check_cols=['store_id', 'Stock_on_hand']  
+        ) 
+    }}
 
 select *
 from {{ ref('int_inventory_with_product') }}
